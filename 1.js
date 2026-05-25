7c897fb220b74c95f25a6829a99d6b52f3b885ea
Aswin Krishna <aswinasus233@gmail.com>
2026-05-25 14:12:57 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+ for(let i=1;i<=5;i++){
+    for(j=i;j<=5;j++){
+        process.stdout.write("  ");
+    }
+     for(j=1;j<=i;j++){
+        process.stdout.write("* ");
+        
+    }
+    
+    console.log("");
+ }
