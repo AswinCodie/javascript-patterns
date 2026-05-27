3fdf7b04dbfb2d156458cd891dbc85cce925b9dd
Aswin Krishna <aswinasus233@gmail.com>
2026-05-27 14:30:52 +0530

    Add files via upload

---
+++ hollow.js
@@ -0,0 +1,12 @@
+
+for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+        if(j==1 || j==5 || i==1 || i==5){
+            process.stdout.write("* ");
+        }
+        else{
+            process.stdout.write("  ")
+        }
+    }
+    console.log("");
+}
